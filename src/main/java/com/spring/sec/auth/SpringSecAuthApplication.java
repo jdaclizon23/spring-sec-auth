f2dd99981366d649d4f3f73b73e4ac00e70a14c8
jov <jovanie.daclizon@gmail.com>
2024-10-27 04:31:09 +0800

    Initialize project

---
+++ src/main/java/com/spring/sec/auth/SpringSecAuthApplication.java
@@ -0,0 +1,11 @@
+package com.spring.sec.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecAuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecAuthApplication.class, args);
+	}
+}
